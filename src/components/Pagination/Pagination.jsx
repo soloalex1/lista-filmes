45af0325507ea43e07f08380ca5a6ca48e23caed
Alexandre Machado <amch.smd@gmail.com>
2021-06-06 18:11:07 -0300

    Finalizado

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,13 +1,7 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const Pagination = ({ page, onChangePage }) => {
-  const pageLength = useSelector((state) => state.moviesList.results?.length);
-  const totalResults = useSelector((state) => state.moviesList?.total_results);
   const totalPages = useSelector((state) => state.moviesList?.total_pages);
-
-  const [minIndex, setMinIndex] = useState(1);
-  const [maxIndex, setMaxIndex] = useState(20);
 
   const nextPage = () => {
     const nextPage = page + 1;
@@ -18,20 +12,12 @@ const Pagination = ({ page, onChangePage }) => {
     const prevPage = page - 1;
     onChangePage(prevPage);
   };
-
-  useEffect(() => {
-    const currentMaxIndex = maxIndex;
-    setMinIndex(currentMaxIndex + 1);
-    setMaxIndex(pageLength * page);
-    // eslint-disable-next-line
-  }, [page]);
-
   return (
     <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
       <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700 font-body">
-            {totalPages && `Página ${page} de ${totalPages}`}
+            {totalPages ? `Página ${page} de ${totalPages}` : null}
           </p>
         </div>
         <div>
