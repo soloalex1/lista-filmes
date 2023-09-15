5c392faf7347712d981628452973ba8d3954aa87
Alexandre Machado <amch.smd@gmail.com>
2023-09-15 12:32:57 -0300

    refactor, remove redux calls from list component

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,28 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 
 import Card from "components/Card";
 
-const List = ({ ...attr }) => {
-  const [lista, setLista] = useState([]);
-
-  const moviesList = useSelector((state) => state.moviesList);
-  const hasMovies = !!moviesList?.results;
-
-  useEffect(() => {
-    if (hasMovies) {
-      setLista(moviesList.results);
-    }
-  }, [moviesList]);
+const List = ({ items, ...attr }) => {
+  const hasMovies = items.length > 0;
 
   return (
     <section
       className="grid grid-cols-1 lg:grid-cols-3 grid-rows-10 lg:grid-rows-6 gap-4 lg:gap-6 px-2 lg:px-4 my-4"
       {...attr}
     >
-      {hasMovies && lista.map((filme) => <Card key={filme.id} movie={filme} />)}
+      {hasMovies && items.map((filme) => <Card key={filme.id} movie={filme} />)}
     </section>
   );
 };
