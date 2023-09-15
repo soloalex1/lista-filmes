538beaf703fd13e02d01a225c0a8a122aefa1187
Alexandre Machado <amch.smd@gmail.com>
2023-09-15 12:32:40 -0300

    refactor, change API calls to async

--- src/api.js
+++ src/api.js
@@ -1,22 +1,21 @@
 const KEY = "c391f33dc3ceb9d568d495ecc681876d";
-const BASE_URL = "https://api.themoviedb.org/3/";
+const BASE_URL = "https://api.themoviedb.org/3";
 
-const client = (endpoint) => {
+const client = async (endpoint) => {
   const options = {
     method: "GET",
   };
 
-  return fetch(
-    `${BASE_URL}${endpoint}api_key=${KEY}&language=pt-BR`,
+  const response = await fetch(
+    `${BASE_URL}/${endpoint}api_key=${KEY}&language=pt-BR`,
     options
-  ).then(async (response) => {
-    if (response.ok) {
-      return await response.json();
-    } else {
-      const errorMessage = await response.text();
-      return Promise.reject(new Error(errorMessage));
-    }
-  });
+  );
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const errorMessage = await response.text();
+    return Promise.reject(new Error(errorMessage));
+  }
 };
 
 export default client;
