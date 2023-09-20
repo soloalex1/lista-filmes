14d8eef463c8b8ef4ef055a9b3eb82f3fbd7d2ef
Alexandre Machado <amch.smd@gmail.com>
2023-09-19 22:24:40 -0300

    refactor, change search to ts

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState, ChangeEvent } from "react";
+import { useHistory } from "react-router";
+
+import useStore from "store";
+import client from "api";
+
+import useDebounce from "hooks/useDebounce";
+
+import { ReactComponent as SearchIcon } from "assets/search.svg";
+
+interface PathnameState {
+  from: string;
+}
+
+const Search = () => {
+  const history = useHistory<PathnameState>();
+
+  const [queryString, setQueryString] = useState("");
+
+  const { setMovieList } = useStore((state) => state);
+
+  const debounceQuery = useDebounce(queryString);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQueryString(e.target.value);
+  };
+
+  const movieSearch = async (query: string) => {
+    const currentPath = history.location.pathname;
+    const previousPath = history.location?.state?.from;
+
+    if (currentPath === "/") {
+      const mountedURL =
+        query === "" && previousPath !== "/movie/:id"
+          ? "trending/movie/day?sort_by=popularity.desc&page=1&"
+          : `search/movie?query=${query}&`;
+
+      await client(mountedURL)
+        .then((data) => {
+          setMovieList(data);
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+
+      return;
+    }
+
+    if (query.trim().length !== 0) {
+      client(`search/movie?query=${query}&`)
+        .then((data) => {
+          setMovieList(data);
+        })
+        .then(() => {
+          history.push("/", { from: "/movie/:id" });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      return;
+    }
+  };
+
+  // if (currentPath === "/") {
+  //   // se estiver na página inicial e não tiver sido redirecionado
+  //   if (query === "" && previousPath !== "/movie/:id") {
+  //     // se a consulta estiver vazia, traz a pesquisa inicial
+  //     client("trending/movie/day?sort_by=popularity.desc&page=1&")
+  //       .then((data) => {
+  //         setMovieList(data);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   } else {
+  //     // se a consulta não estiver vazia, traz a pesquisa baseada na consulta
+  //     client(`search/movie?query=${query}&`)
+  //       .then((data) => {
+  //         setMovieList(data);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   }
+  // } else {
+  //   // se estiver na página de filme
+  //   if (query.trim().length !== 0) {
+  //     console.log("consulta não tá vazia");
+  //     // se a consulta não estiver vazia, faz a consulta normal
+  //     client(`search/movie?query=${query}&`)
+  //       .then((data) => {
+  //         setMovieList(data);
+  //       })
+  //       .then(() => {
+  //         // redireciona para a página inicial
+  //         history.push("/", { from: "/movie/:id" });
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   }
+  // }
+
+  useEffect(() => {
+    movieSearch(debounceQuery);
+  }, [movieSearch, debounceQuery]);
+
+  return (
+    <div className="inline-flex flex-col justify-center text-black w-full">
+      <div className="relative w-full ">
+        <input
+          type="text"
+          className="w-full p-2 pl-10 rounded border border-gray-200 bg-opacity-40 bg-white backdrop-filter backdrop-blur-xl"
+          placeholder="Procure um filme..."
+          value={queryString}
+          onChange={handleChange}
+        />
+        <SearchIcon className="w-4 h-4 absolute left-2.5 top-3.5 fill-current" />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
