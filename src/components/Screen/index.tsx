1d11c78309ed183d1329b81dedb8b98af5438983
Alexandre Machado <amch.smd@gmail.com>
2023-09-19 22:45:35 -0300

    refactor: change screen to typescript

---
+++ src/components/Screen/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Header from "components/Header";
+
+type ScreenProps = {
+  title: string;
+  renderArrow: boolean;
+  children: JSX.Element | JSX.Element[];
+};
+
+const Screen = ({ title = "", children, renderArrow = false }: ScreenProps) => {
+  return (
+    <>
+      <Header name={title} renderArrow={renderArrow} />
+      <main className="w-full h-full mt-2">{children}</main>
+    </>
+  );
+};
+
+export default Screen;
