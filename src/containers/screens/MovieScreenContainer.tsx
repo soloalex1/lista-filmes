e2c05d1f78d0c5ecdfa3dd62f1cbfa29aae9eff8
Alexandre Machado <amch.smd@gmail.com>
2023-09-22 21:11:33 -0300

    refactor: change movie screen to typescript

--- src/containers/screens/MovieScreenContainer.tsx
+++ src/containers/screens/MovieScreenContainer.tsx
@@ -1,56 +1,63 @@
 import React, { useEffect, useState } from "react";
 
-import Screen from "../../components/Screen";
-import client from "../../api";
+import Chip from "components/Chip";
+import Screen from "components/Screen";
+import VideoCard from "components/VideoCard";
 
-import Chip from "../../components/Chip";
-import VideoCard from "../../components/VideoCard";
+import { fetchMovieDetails } from "api";
 
-const MovieScreenContainer = ({ match }) => {
-  const { id } = match.params;
+import { CastEntry, Movie } from "types";
 
-  const [filme, setFilme] = useState({});
+type MovieScreenProps = {
+  match: {
+    params: {
+      id: string;
+    };
+  };
+};
 
-  const [elenco, setElenco] = useState([]);
-
-  const [diretores, setDiretores] = useState("");
-
-  useEffect(() => {
-    client(`/movie/${id}?append_to_response=videos,credits&`)
-      .then((data) => {
-        setFilme(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    //eslint-disable-next-line
-  }, []);
+const MovieScreenContainer = ({
+  match: {
+    params: { id },
+  },
+}: MovieScreenProps) => {
+  const [movie, setMovie] = useState<Movie>();
+  const [cast, setCast] = useState<CastEntry[]>([]);
+  const [directors, setDirectors] = useState("");
 
   useEffect(() => {
-    setElenco(
-      filme?.credits?.cast.filter((member) => {
-        return member.order < 5 && member.known_for_department === "Acting";
-      })
-    );
-
-    const directorList = filme?.credits?.crew.filter((member) => {
-      return member.job === "Director";
+    fetchMovieDetails(id).then((data) => {
+      setMovie(data);
     });
+  }, [fetchMovieDetails]);
 
-    if (directorList?.length === 1) {
-      setDiretores(directorList[0].name);
-    } else {
-      setDiretores(directorList?.map((i) => i.name)?.join(", "));
+  useEffect(() => {
+    if (movie) {
+      setCast(
+        movie?.credits?.cast.filter(
+          ({ order, known_for_department }) =>
+            order < 5 && known_for_department === "Acting"
+        )
+      );
+
+      const directorList = movie.credits.crew.filter(
+        ({ job }) => job === "Director"
+      );
+
+      setDirectors(
+        directorList.length === 1
+          ? directorList[0].name
+          : directorList.map(({ name }) => name).join(", ")
+      );
     }
-  }, [filme]);
+  }, [movie]);
 
   return (
-    <Screen arrowBack>
+    <Screen renderArrow>
       <section
         className="absolute top-0 left-0 w-screen h-auto md:h-full text-white bg-cover bg-full bg-center "
         style={{
-          backgroundImage: `url(https://image.tmdb.org/t/p/original/${filme?.backdrop_path})`,
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
         }}
       >
         {/* grid da página toda */}
@@ -62,15 +69,17 @@ const MovieScreenContainer = ({ match }) => {
           <div className="col-span-4 row-span-2 md:row-start-2 bg-opacity-80 flex justify-start items-center px-4 md:px-8">
             <div className="">
               <h1 className="font-title font-bold text-2xl md:text-5xl flex-grow  justify-self-end leading-normal">
-                {filme?.title}
+                {movie?.title}
               </h1>
               {/* todo: flexbox do título e tagline */}
               <h2 className="font-body flex-grow-0 text-md md:text-lg text-gray-300">
-                {filme?.tagline}
+                {movie?.tagline}
               </h2>
-              <h4>{`${new Date(
-                filme?.release_date
-              ).getFullYear()} | ${diretores}`}</h4>
+              {movie?.release_date && (
+                <h4>{`${new Date(
+                  movie.release_date
+                ).getFullYear()} | ${directors}`}</h4>
+              )}
             </div>
           </div>
 
@@ -88,7 +97,7 @@ const MovieScreenContainer = ({ match }) => {
                 Sinopse
               </h3>
               <p className="font-body text-justify text-sm md:text-md line-clamp-4 lg:line-clamp-10">
-                {filme?.overview}
+                {movie?.overview}
               </p>
             </div>
             <div
@@ -99,7 +108,7 @@ const MovieScreenContainer = ({ match }) => {
                 Elenco
               </h3>
               <div className="font-body flex w-full flex-row flex-wrap">
-                {elenco?.map((i) => (
+                {cast?.map((i) => (
                   <Chip key={i.id} label={i.name} />
                 ))}
               </div>
@@ -109,21 +118,22 @@ const MovieScreenContainer = ({ match }) => {
                 Gênero
               </h3>
               <div className="flex flex-row flex-wrap justify-start items-center w-full">
-                {filme?.genres?.map((genre) => (
+                {movie?.genres?.map((genre) => (
                   <Chip key={genre.id} label={genre.name} />
                 ))}
               </div>
             </div>
           </div>
-          {filme.videos &&
-            filme.videos.results &&
-            filme.videos.results.length > 0 && (
+          {movie &&
+            movie.videos &&
+            movie.videos.results &&
+            movie.videos.results.length > 0 && (
               <div className="col-span-4 md:col-span-2 row-span-2 md:col-start-4 row-start-5 py-2 px-4 flex flex-col justify-end ">
                 <h3 className="font-title font-bold text-md md:text-xl mb-2">
                   Vídeos Relacionados
                 </h3>
                 <div className="w-full h-auto grid gap-2 grid-cols-2 grid-rows-auto place-content-end">
-                  {filme?.videos?.results.map((video) => (
+                  {movie?.videos?.results.map((video) => (
                     <VideoCard video={video} />
                   ))}
                 </div>
