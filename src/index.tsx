b919627c48169798dc165b41f183e2527f4cd1ab
Alexandre Machado <amch.smd@gmail.com>
2023-09-22 21:25:13 -0300

    refactor: change index to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import AppContainer from "./containers/AppContainer.tsx";
+import AppContainer from "./containers/AppContainer";
 
 ReactDOM.render(<AppContainer />, document.querySelector('[data-js="app"]'));
