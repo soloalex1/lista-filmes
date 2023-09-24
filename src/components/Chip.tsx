bee35c31410117defece5e67cabec76486121878
Alexandre Machado <amch.smd@gmail.com>
2023-09-23 22:29:36 -0300

    card and chip recreation

---
+++ src/components/Chip.tsx
@@ -0,0 +1,13 @@
+type ChipProps = {
+  label: string;
+};
+
+const Chip = ({ label }: ChipProps) => {
+  return (
+    <div className=" h-2 md:h-4 border-white text-xs md:text-md border-2 rounded-lg text-white  p-3 md:p-4 m-1 inline-flex items-center text-center">
+      {label}
+    </div>
+  );
+};
+
+export default Chip;
