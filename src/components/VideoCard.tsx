8d500d5fa07ac2475759cd065b126c129ba7d6f6
Alexandre Machado <amch.smd@gmail.com>
2023-09-23 22:34:14 -0300

    create videocard component

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { VideoEntry } from "@/types";
+
+type VideoCardProps = {
+  video: Partial<VideoEntry>;
+};
+
+const VideoCard = ({ video = {} }: VideoCardProps) => {
+  const [base, setBase] = useState("");
+
+  useEffect(() => {
+    if (video.site === "YouTube") {
+      setBase("https://www.youtube.com/embed/");
+      return;
+    }
+
+    if (video.site === "Vimeo") {
+      setBase("https://vimeo.com/");
+      return;
+    }
+  }, [video]);
+
+  return (
+    <div className="w-full h-auto bg-opacity-40 bg-black rounded-lg p-2 flex items-center">
+      <iframe
+        className="w-full h-auto my-auto"
+        title={video.name}
+        src={`${base}${video.key}`}
+        allowFullScreen
+      />
+    </div>
+  );
+};
+
+export default VideoCard;
