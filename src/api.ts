a57820fe6c838c049700dc24d3f118c068cd9070
Alexandre Machado <amch.smd@gmail.com>
2023-09-23 21:57:54 -0300

    recreate API

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+const KEY = "c391f33dc3ceb9d568d495ecc681876d";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+import { Movie, MovieResults } from "@/types";
+
+export const fetchMovieList = async (
+  endpoint: string
+): Promise<MovieResults> => {
+  const options = {
+    method: "GET",
+  };
+
+  const response = await fetch(
+    `${BASE_URL}/${endpoint}api_key=${KEY}&language=pt-BR`,
+    options
+  );
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const errorMessage = await response.text();
+    return Promise.reject(new Error(errorMessage));
+  }
+};
+
+export const fetchMovieDetails = async (id: string): Promise<Movie> => {
+  const options = {
+    method: "GET",
+  };
+
+  const response = await fetch(
+    `${BASE_URL}/movie/${id}?append_to_response=videos,credits&api_key=${KEY}&language=pt-BR`,
+    options
+  );
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const errorMessage = await response.text();
+    return Promise.reject(new Error(errorMessage));
+  }
+};
