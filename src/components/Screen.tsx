4c2afb5d5c4634ebf6d19e2a6873bcc19b454775
Alexandre Machado <amch.smd@gmail.com>
2023-09-23 22:30:14 -0300

    screen recreation

---
+++ src/components/Screen.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+
+type ScreenProps = {
+  title?: string;
+  renderArrow?: boolean;
+  children: JSX.Element | JSX.Element[];
+};
+
+const Screen = ({ title = "", children, renderArrow = false }: ScreenProps) => {
+  return (
+    <>
+      <Header name={title} renderArrow={renderArrow} />
+      <main className="w-full h-full mt-2">{children}</main>
+    </>
+  );
+};
+
+export default Screen;
