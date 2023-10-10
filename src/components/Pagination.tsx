119409876c7a1fe0d321aaa5dc45585def9bca8a
Alexandre Machado <amch.smd@gmail.com>
2023-10-10 20:58:23 -0300

    style: change pagination buttons to icons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+
 import useStore from "@/store";
 
 type PaginationProps = {
@@ -34,14 +36,14 @@ const Pagination = ({ page, onChangePage }: PaginationProps) => {
               onClick={prevPage}
               className="px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-body font-medium text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              Anterior
+              <ChevronLeftIcon className="fill-current h-6 w-6" />
             </button>
             <button
               disabled={page === totalPages}
               onClick={nextPage}
               className="px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-body font-medium text-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              Próxima
+              <ChevronRightIcon className="fill-current h-6 w-6" />
             </button>
           </nav>
         </div>
