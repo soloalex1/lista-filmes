e9cf088c68a46db2d587745c0b0c7a5995551859
Alexandre Machado <amch.smd@gmail.com>
2024-04-25 20:27:37 -0300

    chore: remove unused command

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
